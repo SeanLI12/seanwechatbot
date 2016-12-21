07dc7e6b43237d8e406863831430520b5eda6979
Sean <larcenciel12@gmail.com>
2016-12-21 23:49:33 +0800

    save

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-
+var TelegramBot = require('node-telegram-bot-api');
 const token = process.env.token;
 
-var TelegramBot = require('node-telegram-bot-api');
+
 
 // replace the value below with the Telegram token you receive from @BotFather
 
 // Create a bot that uses 'polling' to fetch new updates
-var bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
 
 
 bot.on("text", (message) => {
